c96a185efcb45921a672ff51a5f9e69156879f60
igorh <igorh@DESKTOP-TLLVOMS>
2019-03-19 18:01:35 +0900

    1st commit for front

---
+++ src/main/java/com/igor/front/HomeController.java
@@ -0,0 +1,80 @@
+package com.igor.front;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		logger.info("Welcome home!");
+		return "home";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public String dashboard() {
+		return "dashboard";
+	}
+
+	@RequestMapping(value = "/documentation", method = RequestMethod.GET)
+	public String documentation() {
+		return "documentation";
+	}
+
+	@RequestMapping(value = "/icons", method = RequestMethod.GET)
+	public String icons() {
+		return "icons";
+	}
+
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+	public String map() {
+		return "map";
+	}
+
+	@RequestMapping(value = "/notifications", method = RequestMethod.GET)
+	public String notifications() {
+		return "notifications";
+	}
+
+	@RequestMapping(value = "/rtl", method = RequestMethod.GET)
+	public String rtl() {
+		return "rtl";
+	}
+
+	@RequestMapping(value = "/tables", method = RequestMethod.GET)
+	public String tables() {
+		return "tables";
+	}
+
+	@RequestMapping(value = "/typography", method = RequestMethod.GET)
+	public String typography() {
+		return "typography";
+	}
+
+	@RequestMapping(value = "/upgrade", method = RequestMethod.GET)
+	public String upgrade() {
+		return "upgrade";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String user() {
+		return "user";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>	Hello world!</h1>
+
+<a href="dashboard">dashboard</a>
+<a href="documentation">documentation</a>
+<a href="icons">icons</a>
+<a href="map">map</a>
+<a href="notifications">notifications</a>
+<a href="rtl">rtl</a>
+<a href="tables">tables</a>
+<a href="typography">typography</a>
+<a href="upgrade">upgrade</a>
+<a href="user">user</a>
+
+</body>
+</html>
